298dc3a2f39597c63cdb95431c5efee37c9d31e0
techTomita <tech.tomita@gmail.com>
2022-06-04 06:22:45 +0000

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,14 @@ Rails.application.routes.draw do
   
   resources :messages
 end
+
+# Prefix       Verb   URI Pattern                  Controller#Action
+#         root GET    /                            messages#index
+#     messages GET    /messages(.:format)          messages#index
+#              POST   /messages(.:format)          messages#create
+#  new_message GET    /messages/new(.:format)      messages#new
+# edit_message GET    /messages/:id/edit(.:format) messages#edit
+#      message GET    /messages/:id(.:format)      messages#show
+#              PATCH  /messages/:id(.:format)      messages#update
+#              PUT    /messages/:id(.:format)      messages#update
+#              DELETE /messages/:id(.:format)      messages#destroy
